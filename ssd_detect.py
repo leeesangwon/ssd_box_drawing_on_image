d59669dd2248a0797dc14b0dd8785f810c8e7198
Sangwon Lee <leee.sangwon@gmail.com>
2018-03-08 01:21:51 +0900

    refactoring

--- ssd_detect.py
+++ ssd_detect.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+import glob
+
 plt.rcParams['figure.figsize'] = (10, 10)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 # Make sure that caffe is on the python path:
-caffe_root = '../'  # this file is expected to be in {caffe_root}/examples
+caffe_root = '/home/sangwon/Projects/caffe'  # this file is expected to be in {caffe_root}/examples
 import os
 os.chdir(caffe_root)
 import sys
@@ -20,73 +22,74 @@ caffe.set_mode_gpu()
 from google.protobuf import text_format
 from caffe.proto import caffe_pb2
 
-# load PASCAL VOC labels
-labelmap_file = 'data/VOC0712/labelmap_voc.prototxt'
-file = open(labelmap_file, 'r')
-labelmap = caffe_pb2.LabelMap()
-text_format.Merge(str(file.read()), labelmap)
 
-def get_labelname(labelmap, labels):
-    num_labels = len(labelmap.item)
-    labelnames = []
-    if type(labels) is not list:
-        labels = [labels]
-    for label in labels:
-        found = False
-        for i in xrange(0, num_labels):
-            if label == labelmap.item[i].label:
-                found = True
-                labelnames.append(labelmap.item[i].display_name)
-                break
-        assert found == True
-    return labelnames
+def main(input_folder, output_folder, labelmap_file, model_def, model_weights, image_resize):
+    assert os.path.isdir(input_folder), "input_folder(%s) does not exist" % input_folder
+    assert os.path.isfile(labelmap_file), "labelnap_file(%s) does not exist" % labelmap_file
+    assert os.path.isfile(model_def), "model_def(%s) does not exist" % model_def
+    assert os.path.isfile(model_weights), "model_weights(%s) does not exist" % model_weights
+    
+    try:
+        os.mkdir(output_folder)
+    except:
+        pass
+    draw_bbox_on_images(input_folder, output_folder, labelmap_file, model_def, model_weights, image_resize)
 
-#model_def = 'models/VGGNet/VOC0712/PPSSD_512x512_ar6_coco/deploy.prototxt'
-#model_weights = 'models/VGGNet/VOC0712/PPSSD_512x512_ar6_coco/VGG_VOC2012_PPSSD_512x512_ft_iter_160000.caffemodel'
+    
+def draw_bbox_on_images(input_folder, output_folder, labelmap_file, model_def, model_weights, image_resize):
+    labelmap = get_labelmap(labelmap_file)
+    net = get_net(model_def, model_weights)
+    transformer = get_transformer(net)
 
-model_def = 'models/VGGNet/VOC0712/city/deploy.prototxt'
-model_weights = 'models/VGGNet/city.caffemodel'
+    # set net to batch size of 1
+    net.blobs['data'].reshape(1,3,image_resize,image_resize)
+
+    input_list = glob.glob(os.path.join(input_folder, "*.jpg"))
+    input_list += glob.glob(os.path.join(input_folder, "*.png"))
+    
+    assert input_list, "input_folder(%s) doesn't have image file(jpeg or png format)" % input_folder
+
+    for input_image in input_list:
+        output_image = os.path.join(output_folder, os.path.basename(input_image))
+        draw_bbox_on_image(
+            input_image_path=input_image,
+            output_image_path=output_image,
+            labelmap=labelmap, net=net, transformer=transformer)
 
 
-net = caffe.Net(model_def,      # defines the structure of the model
-                model_weights,  # contains the trained weights
-                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+def get_labelmap(labelmap_file):
+    with open(labelmap_file, 'r') as f:
+        labelmap = caffe_pb2.LabelMap()
+        text_format.Merge(str(f.read()), labelmap)
+    return labelmap
 
-# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
-transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-transformer.set_transpose('data', (2, 0, 1))
-transformer.set_mean('data', np.array([104,117,123])) # mean pixel
-transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
-transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
 
-##############################################################################
-# READ & WRITE
-##############################################################################
-# set net to batch size of 1
-#image_resize = 300
-image_resize = 512
-net.blobs['data'].reshape(1,3,image_resize,image_resize)
+def get_net(model_def, model_weights):
+    return caffe.Net(model_def,      # defines the structure of the model
+                     model_weights,   # contains the trained weights
+                     caffe.TEST)     # use test mode (e.g., don't perform dropout)
 
-#inputlist = open("data/VOC2012/list.txt", 'r') # test image list path
-inputlist = open("examples/cityscapes/list/munster.txt", 'r') # test image list path
-lines = inputlist.readlines()
 
-##############################################################################
-for x in range(len(lines)):
-    preline = lines[x]
-    test_line = preline.split(" ")[0]
-    outline = test_line.split("/")[-1]
+def get_transformer(net):
+    # input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+    transformer.set_transpose('data', (2, 0, 1))
+    transformer.set_mean('data', np.array([104,117,123])) # mean pixel
+    transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+    transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+    return transformer
 
-    #outline = test_line[-11:]
-    #outline = test_line[-16:] # VOC2012
-    #outline = test_line[-40:] # city
 
-    image = caffe.io.load_image("/home/younghyunlab/caffe/examples/cityscapes/" + test_line) # image set path
-    plt.imshow(image)
-    #plt.show() #plot image on screen
+def draw_bbox_on_image(input_image_path, output_image_path, labelmap, net, transformer):
+    image = caffe.io.load_image(input_image_path) # image set path
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_aspect('equal')
+    ax.imshow(image, aspect='equal')
+    ax.axis('off')
+    
     transformed_image = transformer.preprocess('data', image)
     net.blobs['data'].data[...] = transformed_image
-
     # Forward pass.
     detections = net.forward()['detection_out']
     # Parse the outputs.
@@ -110,14 +113,8 @@ for x in range(len(lines)):
 
     colors = plt.cm.hsv(np.linspace(0, 1, 21)).tolist()
 
-    #plt.imshow(image)
-    plt.axis('off')
-
-    # get figure and axis handle 
-    fig = plt.gcf()
-    currentAxis = plt.gca()
     # get axis extent
-    extent = currentAxis.get_window_extent().transformed(fig.dpi_scale_trans.inverted())    
+    extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())    
 
     for i in xrange(top_conf.shape[0]):
         xmin = int(round(top_xmin[i] * image.shape[1]))
@@ -130,11 +127,63 @@ for x in range(len(lines)):
         display_txt = '%s: %.2f'%(label_name, score)
         coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
         color = colors[label]
-        currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
-        currentAxis.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
-
-    #plt.savefig('examples/VOC2012_results/PFPNET_coco' + outline, transparent=True, bbox_inches=extent)
-    plt.savefig('examples/cityscapes_results/munster/' + outline, transparent=True, bbox_inches=extent)
-    currentAxis.clear()
+        ax.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
+        ax.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
+    
+    plt.savefig(output_image_path, transparent=True, bbox_inches=extent)
+    ax.clear()
     
 
+def get_labelname(labelmap, labels):
+    num_labels = len(labelmap.item)
+    labelnames = []
+    if type(labels) is not list:
+        labels = [labels]
+    for label in labels:
+        found = False
+        for i in xrange(0, num_labels):
+            if label == labelmap.item[i].label:
+                found = True
+                labelnames.append(labelmap.item[i].display_name)
+                break
+        assert found == True
+    return labelnames
+
+if __name__=="__main__":
+    # for image_resize in [300, 512]:
+    #     input_folder ="/home/sangwon/Projects/ssd_512_test/target_image"
+    #     output_folder="/home/sangwon/Projects/ssd_512_test/result_image_%s_Plus" % image_resize
+    #     labelmap_file = '/home/sangwon/Projects/caffe/data/VOC0712/labelmap_voc.prototxt'
+    #     model_def = '/home/sangwon/Projects/caffe/models/VGGNet/VOC0712Plus/SSD_%sx%s_ft/deploy.prototxt' % (image_resize, image_resize)
+    #     model_weights = '/home/sangwon/Projects/caffe/models/VGGNet/VOC0712Plus/SSD_%sx%s_ft/VGG_VOC0712Plus_SSD_%sx%s_ft_iter_160000.caffemodel' % (image_resize, image_resize, image_resize, image_resize)
+    #     image_resize = image_resize
+        
+    #     for city in ['frankfurt', 'lindau', 'munster']:
+    #         main(input_folder=os.path.join(input_folder, city),
+    #             output_folder=os.path.join(output_folder, city),
+    #             labelmap_file=labelmap_file,
+    #             model_def=model_def,
+    #             model_weights=model_weights,
+    #             image_resize=image_resize)
+
+    input_folder ="/home/sangwon/Projects/ssd_512_test/target_image"
+    output_folder="/home/sangwon/Projects/ssd_512_test/result_image_300"
+    labelmap_file = '/home/sangwon/Projects/caffe/data/VOC0712/labelmap_voc.prototxt'
+    model_def = '/home/sangwon/Projects/caffe/models/VGGNet/VOC0712/SSD_300x300/deploy.prototxt'
+    model_weights = '/home/sangwon/Projects/caffe/models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_120000.caffemodel'
+    image_resize = 300
+    
+    # main(input_folder=input_folder,
+    #     output_folder=output_folder,
+    #     labelmap_file=labelmap_file,
+    #     model_def=model_def,
+    #     model_weights=model_weights,
+    #     image_resize=image_resize)
+
+    for city in ['frankfurt', 'lindau', 'munster']:
+        main(input_folder=os.path.join(input_folder, city),
+            output_folder=os.path.join(output_folder, city),
+            labelmap_file=labelmap_file,
+            model_def=model_def,
+            model_weights=model_weights,
+            image_resize=image_resize)
